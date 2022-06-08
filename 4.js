6ea681860a5d8dd1f4a456291d2c5b0c0c782a2f
kiyeol <kyday63@gmail.com>
2022-06-08 22:19:05 +0900

    coding test

---
+++ 4.js
@@ -0,0 +1,18 @@
+function solution(s) {
+  const stack = [];
+  for (const char of s) {
+    if (char === "{" || char === "[" || char === "(") {
+      stack.push(char);
+    } else {
+      const lastChar = stack.pop();
+      if (
+        (char === "}" && lastChar !== "{") ||
+        (char === "]" && lastChar !== "[") ||
+        (char === ")" && lastChar !== "(")
+      ) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
