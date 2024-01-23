b2b1cc10fe76764b74ba5455801b8ee2e2c873ed
kyday <kyday63@gmail.com>
2024-01-23 23:31:46 +0900

    20240123 카운트 다운

---
+++ programmers/2024/0123.js
@@ -0,0 +1,22 @@
+/*
+
+정수 start_num와 end_num가 주어질 때, start_num에서 end_num까지 1씩 감소하는 수들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+0 ≤ end_num ≤ start_num ≤ 50
+
+입출력 예
+start_num	end_num	result
+10	3	[10, 9, 8, 7, 6, 5, 4, 3]
+
+*/
+
+function solution(start, end_num) {
+  let answer = [];
+
+  for (let i = 0; i <= start - end_num; i++) {
+    answer.push(start - i);
+  }
+
+  return answer;
+}
