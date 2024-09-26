0d03ec0ec06654ee11994ef5fb285d28bafd74b5
KiYeol Kim <42636378+kyday@users.noreply.github.com>
2024-09-27 00:21:40 +0900

    Time: 107 ms (31.29%), Space: 51.9 MB (90.87%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.ts
@@ -0,0 +1,7 @@
+function twoSum(nums: number[], target: number): number[] {
+     for(let i = 0; i < nums.length - 1; i++) {
+        for(let j = i+1; j < nums.length; j++) {
+            if(target === nums[i] + nums[j]) return [i, j];
+        }
+    }
+};
